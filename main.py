3b4c096d33a001ed5ee8520bfdee94c904055f32
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-01-26 09:31:57 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as ps
+
+label1 = ps.Text('Enter feet:')
+label2 = ps.Text('Enter inches:')
+input1 = ps.InputText(key='feet')
+input2 = ps.InputText(key='inches')
+button = ps.Button('Convert', key='convert_button')
+action_text = ps.Text('', key='action_text')
+
+window = ps.Window('Feet Inches to Meters',
+                   layout=[
+                       [label1],
+                       [input1],
+                       [label2],
+                       [input2],
+                       [button, action_text]
+                   ])
+
+while True:
+    event, values = window.read()
+
+window.close()
